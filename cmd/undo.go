6e1b75616feb964fc7ff8c9951a651168d63d3a0
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2023-01-24 21:42:09 +0900

    非同期処理の部分を共通化させるためにundoコマンドに対してinterfaceを実装

--- cmd/undo.go
+++ cmd/undo.go
@@ -1,65 +1,25 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/rikutkb/url_command.git/cmd/undo"
 	"github.com/spf13/cobra"
-	"golang.org/x/sync/errgroup"
 )
-
-func fetchUrls(ctx context.Context, reqUrls []string) (error, map[string]string) {
-	sem := make(chan bool, 3)
-	eg, ctx := errgroup.WithContext(ctx)
-	defer close(sem)
-	var urlPairs = make(map[string]string)
-
-	for _, url := range reqUrls {
-
-		sem <- true
-		func(_url string) {
-			eg.Go(func() error {
-				select {
-				case <-ctx.Done():
-					return fmt.Errorf("キャンセルされました: %s", ctx.Err())
-				default:
-				}
-				defer func() {
-					<-sem
-				}()
-				longUrl, err := undo.GetRedirect(ctx, _url)
-				if err != nil {
-					return err
-				}
-				urlPairs[_url] = longUrl
-				return nil
-			})
-		}(url)
-
-	}
-	if err := eg.Wait(); err != nil {
-		SetCancelSignal(err.Error())
-		return err, nil
-	}
-	return nil, urlPairs
-}
 
 var undoCmd = &cobra.Command{
 	Use:   "undo",
 	Short: "短縮化されたurlを元に戻します。",
 	Long:  "",
 	Run: func(cmd *cobra.Command, args []string) {
-		splitedUrl := strings.Split(urls, ",")
-		if err, pairUrls := fetchUrls(ctx, splitedUrl); err != nil {
+		splitedUrls := strings.Split(urls, ",")
+		sfc := &undo.UndoFetchCommand{}
+		err := resolveUrls(ctx, splitedUrls, sfc)
+		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(2)
-		} else {
-			for baseUrl, resultUrl := range pairUrls {
-				fmt.Fprintln(os.Stdout, "baseUrl:"+baseUrl+",resultUrl: "+resultUrl)
-			}
 		}
 	},
 }
--- cmd/undo/undo.go
+++ cmd/undo/undo.go
@@ -60,39 +60,3 @@ func (ufc *UndoFetchCommand) GetData(ctx context.Context, url string) error {
 func (ufc UndoFetchCommand) WriteData() error {
 	return nil
 }
-
-func GetRedirect(ctx context.Context, url string) (longUrl string, err error) {
-	client := &http.Client{}
-	// goの標準のリダイレクト機能をオーバライド
-	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-		return http.ErrUseLastResponse
-	}
-	req, err := http.NewRequest("GET", url, nil)
-	req = req.WithContext(ctx)
-	if err != nil {
-		return "", fmt.Errorf("リクエスト作成中にエラーが起きました: %s", err)
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", fmt.Errorf("通信中にエラーが起きました: %s", err)
-	}
-	if resp.StatusCode != http.StatusMovedPermanently {
-		return "", fmt.Errorf("通信中にエラーが起きました: status code :%d", resp.StatusCode)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		return "", err
-	}
-	reg := regexp.MustCompile(`(<a href=")(.*)(">.*)(</a>)`)
-	parsed := reg.FindStringSubmatch(string(body))
-	if len(parsed) < 4 {
-		return "", fmt.Errorf("レスポンスのパースに失敗しました。")
-	}
-	return parsed[2], nil
-	// <a href="https://xxxxxxxxxxxxxxxxxxx">https://xxxxxxxxxxxxxxxxxxx</a>
-	// <a href="https://xxxxxxxxxxxxxxxxxxx">
-	// https://xxxxxxxxxxxxxxxxxxx
-	// </a>
-}
